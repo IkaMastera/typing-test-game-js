27a313dd557b10492001f2ad4879ce22f2b1cfe0
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-05-22 15:47:31 +0400

    add: stop and dispalying the result functionality

--- script.js
+++ script.js
@@ -49,10 +49,41 @@ userInput.addEventListener("input", () => {
   });
 });
 
+function updateTimer() {
+  if (time == 0) {
+    displayResult();
+  } else {
+    document.getElementById("timer").innerText = --time + "s";
+  }
+}
+
+const timeReduce = () => {
+  time = 60;
+  timer = setInterval(updateTimer, 1000);
+};
+
+const displayResult = () => {
+  document.querySelector(".result").style.display = "block";
+  clearInterval(timer);
+  document.getElementById("stop-test").style.display = "none";
+  userInput.disabled = true;
+  let timeTaken = 1;
+  if (time != 0) {
+    timeTaken = (60 - time) / 100;
+  }
+  document.getElementById("wpm").innerText =
+    (userInput.value.length / 5 / timeTaken).toFixed(2) + "wpm";
+  document.getElementById("accuracy").innerText =
+    Math.round(
+      ((userInput.value.length - mistakes) / userInput.value.length) * 100
+    ) + "%";
+};
+
 const startTest = () => {
   mistakes = 0;
   timer = "";
   userInput.disabled = false;
+  timeReduce();
   document.getElementById("start-test").style.display = "none";
   document.getElementById("stop-test").style.display = "block";
 };
